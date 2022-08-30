18c96012be06049ca296cae2d1ad3523e741847b
NASEEM A P <naseemalassampattil@gmail.com>
2022-08-30 16:55:30 +0530

    added Normalisation, Predicting Correctly

--- inference.py
+++ inference.py
@@ -1,6 +1,7 @@
 from keras.models import load_model
 from mtcnn import MTCNN
 from my_utils import alignment_procedure
+from sklearn.preprocessing import MinMaxScaler
 import ArcFace
 import cv2
 import numpy as np
@@ -56,22 +57,29 @@ while True:
             img_embedding = arcface_model.predict(img_resize)[0]
 
             data = pd.DataFrame([img_embedding], columns=np.arange(512))
+
+            # Normalise
+            x = data.values #returns a numpy array
+            min_max_scaler = MinMaxScaler()
+            x_scaled = min_max_scaler.fit_transform(x)
+            data = pd.DataFrame(x_scaled)
+
             predict = face_rec_model.predict(data)[0]
             print(predict)
-            # pose_class = class_names[predict.argmax()]
+            pose_class = class_names[predict.argmax()]
 
             # Show Result
-            # img = cv2.putText(
-            #     img, f'{pose_class}',
-            #     (40, 50), cv2.FONT_HERSHEY_PLAIN,
-            #     2, (255, 0, 255), 2
-            # )
+            img = cv2.putText(
+                img, f'{pose_class}',
+                (40, 50), cv2.FONT_HERSHEY_PLAIN,
+                2, (255, 0, 255), 2
+            )
 
     else:
         print('[INFO] Eyes Not Detected!!')
 
-    # cv2.imshow('Output Image', img)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     cv2.destroyAllWindows()
-    #     break
+    cv2.imshow('Output Image', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 print('[INFO] Inference on Videostream is Ended...')
