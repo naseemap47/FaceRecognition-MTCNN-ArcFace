376395f3ea1f579190da39fcc7ddc2acc506e9c7
NASEEM A P <naseemalassampattil@gmail.com>
2022-08-30 12:48:16 +0530

    detecter box added after Image saved

--- take_imgs.py
+++ take_imgs.py
@@ -1,20 +1,13 @@
-import os
 import cv2
 import argparse
 
 
 ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--dataset", type=str, required=True,
-#                 help="path to dataset/dir")
-# ap.add_argument("-c", "--classes", type=str, required=True,
-#                 help="path to classes.txt")
 ap.add_argument("-o", "--save", type=str, required=True,
                 help="path to save dir")
 
 
 args = vars(ap.parse_args())
-# path_to_dir = args["dataset"]
-# path_to_txt = args['classes']
 path_to_save = args['save']
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -27,6 +20,11 @@ while True:
         print('[INFO] Cam NOT working!!')
         break
 
+    # Save Image
+    cv2.imwrite(f'{path_to_save}/{count}.jpg', img)
+    print(f'[INFO] Successfully Saved {count}.jpg')
+    count += 1
+
     faces = face_classifier.detectMultiScale(img)
 
     for (x, y, w, h) in faces:
@@ -34,15 +32,10 @@ while True:
             img, (x, y), (x+w, y+h),
             (0, 255, 0), 2
         )
-        img_roi = img[y:y+h, x:x+w]
-        cv2.imwrite(f'{path_to_save}/{count}.jpg', img_roi)
-        print(f'[INFO] Successfully Saved {count}.jpg')
-        count += 1
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
-    if count == 50:
-        print('[INFO] Collected 50 Images')
+    if count == 100:
+        print('[INFO] Collected 100 Images')
         cv2.destroyAllWindows()
         break
-
