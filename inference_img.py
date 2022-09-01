9b024900323aa2c63fb99e501bac140b0ace072a
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-01 16:01:50 +0530

    added bbox and putText on prediction

--- inference_img.py
+++ inference_img.py
@@ -9,15 +9,24 @@ import pandas as pd
 import argparse
 
 
+######################
+class_names = [
+    'Naseem', 'Vikas'
+]
+######################
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
                 help="path to image")
 ap.add_argument("-m", "--model", type=str, required=True,
                 help="path to saved .h5 model, eg: dir/model.h5")
+ap.add_argument("-c", "--conf", type=float, default=0.9,
+                help="min prediction conf (0<conf<1)")
 
 args = vars(ap.parse_args())
 path_to_img = args["image"]
 path_saved_model = args["model"]
+threshold = args["conf"]
 
 # Load saved model
 face_rec_model = load_model(path_saved_model, compile=True)
@@ -25,10 +34,8 @@ face_rec_model = load_model(path_saved_model, compile=True)
 detector = MTCNN()
 
 arcface_model = ArcFace.loadModel()
-arcface_model.load_weights("arcface_weights.h5")
+# arcface_model.load_weights("arcface_weights.h5")
 target_size = arcface_model.layers[0].input_shape[0][1:3]
-
-class_names = ['Naseem', 'Vikas']
 
 img = cv2.imread(path_to_img)
 detections = detector.detect_faces(img)
@@ -38,6 +45,8 @@ if len(detections) > 0:
         right_eye = detect['keypoints']['right_eye']
         left_eye = detect['keypoints']['left_eye']
         bbox = detect['box']
+        xmin, ymin, xmax, ymax = int(bbox[0]), int(bbox[1]), \
+                    int(bbox[2]+bbox[0]), int(bbox[3]+bbox[1])
         norm_img_roi = alignment_procedure(img, left_eye, right_eye, bbox)
 
         img_resize = cv2.resize(norm_img_roi, target_size)
@@ -50,13 +59,19 @@ if len(detections) > 0:
         data = pd.DataFrame([img_embedding], columns=np.arange(512))
 
         predict = face_rec_model.predict(data)[0]
-        print(predict)
-        pose_class = class_names[predict.argmax()]
-
+        if max(predict) > threshold:
+            pose_class = class_names[predict.argmax()]
+        else:
+            pose_class = 'Unkown Person'
+        
         # Show Result
+        cv2.rectangle(
+            img, (xmin, ymin), (xmax, ymax),
+            (0, 255, 0), 2
+        )
         cv2.putText(
             img, f'{pose_class}',
-            (40, 50), cv2.FONT_HERSHEY_PLAIN,
+            (xmin, ymin-10), cv2.FONT_HERSHEY_PLAIN,
             2, (255, 0, 255), 2
         )
 
