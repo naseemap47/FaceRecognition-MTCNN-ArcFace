0fb870de757ecf6dd6cf0f54bb9523f5f35bd9ea
NASEEM A P <naseemalassampattil@gmail.com>
2023-01-20 17:17:27 +0530

    added label encoder for storing poeple names - le.pickle

--- train.py
+++ train.py
@@ -6,22 +6,30 @@ import numpy as np
 import keras
 from keras import layers, Sequential
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import pickle
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", type=str, required=True,
-                help="path to dataset/dir")
-ap.add_argument("-o", "--save", type=str, required=True,
+ap.add_argument("-i", "--dataset", type=str, default='Norm',
+                help="path to Norm/dir")
+ap.add_argument("-o", "--save", type=str, default='models/model.h5',
                 help="path to save .h5 model, eg: dir/model.h5")
+ap.add_argument("-l", "--le", type=str, default='models/le.pickle',
+	            help="path to label encoder")
+ap.add_argument("-b", "--batch_size", type=int, default=16,
+	            help="batch Size for model training")
+ap.add_argument("-e", "--epochs", type=int, default=200,
+	            help="Epochs for Model Training")
 
 
 args = vars(ap.parse_args())
 path_to_dir = args["dataset"]
-path_to_save = args['save']
+checkpoint_path = args['save']
 
 # Load ArcFace Model
 model = ArcFace.loadModel()
@@ -48,7 +56,7 @@ for name in names:
         img = cv2.imread(img_path)
         img_resize = cv2.resize(img, target_size)
         # what this line doing? must?
-        img_pixels = image.img_to_array(img_resize)
+        img_pixels = tf.keras.preprocessing.image.img_to_array(img_resize)
         img_pixels = np.expand_dims(img_pixels, axis=0)
         img_norm = img_pixels/255  # normalize input in [0, 1]
         img_embedding = model.predict(img_norm)[0]
@@ -61,16 +69,15 @@ print('[INFO] Image Data Embedding Completed...')
 
 # DataFrame
 df = pd.DataFrame(x, columns=np.arange(512))
-df['names'] = y
-
 x = df.copy()
-y = x.pop('names')
-y, _ = y.factorize()
 x = x.astype('float64')
-y = keras.utils.to_categorical(y)
+
+le = LabelEncoder()
+labels = le.fit_transform(y)
+labels = tf.keras.utils.to_categorical(labels, 2)
 
 # Train Deep Neural Network
-x_train, x_test, y_train, y_test = train_test_split(x, y,
+x_train, x_test, y_train, y_test = train_test_split(x, labels,
                                                     test_size=0.2,
                                                     random_state=0)
 
@@ -91,7 +98,6 @@ model.compile(
 
 # Add a checkpoint callback to store the checkpoint that has the highest
 # validation accuracy.
-checkpoint_path = path_to_save
 checkpoint = keras.callbacks.ModelCheckpoint(checkpoint_path,
                                              monitor='val_accuracy',
                                              verbose=1,
@@ -103,13 +109,19 @@ earlystopping = keras.callbacks.EarlyStopping(monitor='val_accuracy',
 print('[INFO] Model Training Started ...')
 # Start training
 history = model.fit(x_train, y_train,
-                    epochs=200,
-                    batch_size=16,
+                    epochs=args['epochs'],
+                    batch_size=args['batch_size'],
                     validation_data=(x_test, y_test),
                     callbacks=[checkpoint, earlystopping])
 
 print('[INFO] Model Training Completed')
-print(f'[INFO] Model Successfully Saved in /{path_to_save}')
+print(f'[INFO] Model Successfully Saved in /{checkpoint_path}')
+
+# save label encoder
+f = open(args["le"], "wb")
+f.write(pickle.dumps(le))
+f.close()
+print('[INFO] Successfully Saved models/le.pickle')
 
 # Plot History
 metric_loss = history.history['loss']
